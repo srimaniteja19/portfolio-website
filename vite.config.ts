071bdca0e095281cd8fc3c203bb7a10304fcb29f
Prince Raj <pr8102232@gmail.com>
2026-03-04 19:21:33 +0530

    first

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Sri_Maniteja/',
+    base: '/portfolio-website/',
     plugins: [react()],
     resolve: {
         alias: {
